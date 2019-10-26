64bcbdd7a19d3dd8a068fca3ab2d089f8581cc79
Dieter Luypaert <dieterluypaert@gmail.com>
2019-10-26 12:37:12 +0200

    Chore: Remove unnecessary code

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,15 +1,7 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-import React from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
-import { useStaticQuery, graphql } from 'gatsby';
+import React from 'react';
 
-import Header from './header';
 import './layout.css';
 
 const Layout = ({ children }) => {
@@ -25,7 +17,21 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <header style={{ background: `rebeccapurple`, marginBottom: `1.45rem` }}>
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `1.45rem 1.0875rem`,
+          }}
+        >
+          <h1 style={{ margin: 0 }}>
+            <Link to="/" style={{ color: `white`, textDecoration: `none` }}>
+              {data.site.siteMetadata.title}
+            </Link>
+          </h1>
+        </div>
+      </header>
       <div
         style={{
           margin: `0 auto`,
--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -1,14 +1,7 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-import React from 'react';
-import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Helmet from 'react-helmet';
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
@@ -29,44 +22,18 @@ function SEO({ description, lang, meta, title }) {
 
   return (
     <Helmet
-      htmlAttributes={{
-        lang,
-      }}
+      htmlAttributes={{ lang }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
+        { content: metaDescription, name: `description` },
+        { content: title, property: `og:title` },
+        { content: metaDescription, property: `og:description` },
+        { content: `website`, property: `og:type` },
+        { content: `summary`, name: `twitter:card` },
+        { content: site.siteMetadata.author, name: `twitter:creator` },
+        { content: title, name: `twitter:title` },
+        { content: metaDescription, name: `twitter:description` },
       ].concat(meta)}
     />
   );
