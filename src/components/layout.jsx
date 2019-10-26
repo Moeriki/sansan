c8d87223e5c0ba5374d5fc36e2fa52eb2bc50bb2
Dieter Luypaert <dieterluypaert@gmail.com>
2019-10-26 14:44:41 +0200

    Feat: Add social media links

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,4 +1,7 @@
-import { Link, useStaticQuery, graphql } from 'gatsby';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/pro-solid-svg-icons';
+import { useStaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 
@@ -18,17 +21,38 @@ const Layout = ({ children }) => {
   return (
     <>
       <header className="bg-white">
-        <div className="container mx-auto">
+        <div className="container mx-auto mt-6">
           <h1 className="text-center">
-            <Link className="block py-6" to="/">
-              <span className="font-title text-2xl">
-                {data.site.siteMetadata.title}
-              </span>
-            </Link>
+            <span className="font-title text-2xl">
+              {data.site.siteMetadata.title}
+            </span>
           </h1>
+          <div className="mt-1 text-center">
+            <a
+              href="https://www.facebook.com/sandycroesphotography"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <FontAwesomeIcon className="mx-3" icon={faFacebookF} />
+            </a>
+            <a
+              href="https://instagram.com/sandycroes"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <FontAwesomeIcon className="mx-3" icon={faInstagram} />
+            </a>
+            <a
+              href="mailto:info@sandycroes.be"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <FontAwesomeIcon className="mx-3" icon={faEnvelope} />
+            </a>
+          </div>
         </div>
       </header>
-      <div className="bg-scp-white p-6">
+      <div className="bg-scp-white p-6 mt-6">
         <main className="container mx-auto">{children}</main>
       </div>
     </>
