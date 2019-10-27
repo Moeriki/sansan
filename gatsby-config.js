0915eb6fd04009923688b15da0300314e753eb64
Dieter Luypaert <dieterluypaert@gmail.com>
2019-10-27 16:21:40 +0100

    Feat: Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: '#FFF',
         theme_color: '#FFF',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
