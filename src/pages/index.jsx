a822c9a15dd5fbcde42e4e119425005fdc4d279b
Dieter Luypaert <dieterluypaert@gmail.com>
2021-10-16 17:18:56 +0200

    remove avatar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 import { graphql, useStaticQuery } from 'gatsby';
-import Image from 'gatsby-image';
 import React from 'react';
 
 import Layout from '../components/layout';
@@ -10,30 +9,15 @@ import styles from './remark.module.css';
 export default function IndexPage() {
   const data = useStaticQuery(graphql`
     query {
-      sandyAvatarFile: file(name: { eq: "avatar" }) {
-        childImageSharp {
-          fixed(quality: 90, width: 100) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
       introRemark: markdownRemark(fileAbsolutePath: { glob: "**/intro.md" }) {
         html
       }
     }
   `);
-  const { introRemark, sandyAvatarFile } = data;
+  const { introRemark } = data;
   return (
     <Layout>
       <SEO title="Home" />
-      <div className="my-12 text-center">
-        <Image
-          alt="Portret van Sandy Croes"
-          className="inline-block rounded-full"
-          fixed={sandyAvatarFile.childImageSharp.fixed}
-          style={{ width: 100 }}
-        />
-      </div>
       <div
         className={`my-12 leading-relaxed lg:leading-loose text-center text-lg lg:text-xl ${styles.content}`}
         dangerouslySetInnerHTML={{ __html: introRemark.html }}
